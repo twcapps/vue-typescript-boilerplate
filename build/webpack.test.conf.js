8a1f50356428141c7f68b9c0b98a883ee95728ec
Bart van den Ende <bartvandenende@twcapps.com>
2017-06-30 09:46:49 +0200

    resolve some issues with unit test setup

---
+++ build/webpack.test.conf.js
@@ -0,0 +1,35 @@
+// This is the webpack config used for unit tests.
+require('es6-promise').polyfill();
+var utils = require('./utils')
+var webpack = require('webpack')
+var merge = require('webpack-merge')
+var baseConfig = require('./webpack.base.conf')
+
+var FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
+
+var webpackConfig = merge(baseConfig, {
+  // use inline sourcemap for karma-sourcemap-loader
+  module: {
+    rules: utils.styleLoaders()
+  },
+  devtool: '#inline-source-map',
+  resolveLoader: {
+    alias: {
+      // necessary to to make lang="scss" work in test when using vue-loader's ?inject option 
+      // see discussion at https://github.com/vuejs/vue-loader/issues/724
+      'scss-loader': 'sass-loader'
+    }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': require('../config/test.env')
+    })
+    // new webpack.NoEmitOnErrorsPlugin(),
+    // new FriendlyErrorsPlugin()
+  ]
+})
+
+// no need for app entry during tests
+delete webpackConfig.entry
+
+module.exports = webpackConfig
--- test/unit/index.js
+++ test/unit/index.js
@@ -1,13 +1,12 @@
-// Polyfill fn.bind() for PhantomJS
-/* eslint-disable no-extend-native */
-Function.prototype.bind = require('function-bind')
+import Vue from 'vue'
+Vue.config.productionTip = false
 
-// require all test files (files that ends with .spec.js)
-const testsContext = require.context('./specs', true, /\.spec$/)
+// require all test files (files that ends with .spec.ts)
+const testsContext = require.context('../../src', true, /\.spec$/)
 testsContext.keys().forEach(testsContext)
 
-// require all src files except main.js for coverage.
+// require all src files except main.ts for coverage.
 // you can also change this to match only the subset of files that
 // you want coverage for.
-const srcContext = require.context('../../src', true, /^\.\/(?!main(\.js)?$)/)
+const srcContext = require.context('../../src', true, /^\.\/(?!main\.ts$).+\.(ts|vue)$/i)
 srcContext.keys().forEach(srcContext)
