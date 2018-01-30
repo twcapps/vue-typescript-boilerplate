8452235d6a2bbc886e080fa43f5f39aa6cce6083
Morten Broesby-Olsen <morten@twcapps.com>
2018-01-30 16:30:20 +0100

    Minor cleanup in view

--- template/src/components/navbar/navbar.vue
+++ template/src/components/navbar/navbar.vue
@@ -3,9 +3,10 @@
   <div class="container">
     <div class="navbar-header">
       <router-link to="/" class="navbar-brand">
-        <img src="../../assets/images/logo.png" alt="brand" class="img-responsive"> {{{{raw-helper}}}}{{ "navbar-title" | format-message }}{{{{/raw-helper}}}}
+        <img src="../../assets/images/logo.png" alt="brand" class="img-responsive" />
+        <span>{{{{raw-helper}}}}{{ "navbar-title" | format-message }}{{{{/raw-helper}}}}</span>
       </router-link>
-      <p v-on:click="onClickLoginDemo()">{{ loginButtonText }}</p>
+      <p v-on:click="onClickLoginDemo()">{{{{raw-helper}}}}{{ loginButtonText }}{{{{/raw-helper}}}}</p>
     </div>
   </div>
 </nav>
