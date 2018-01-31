e3338869bd2688dc88aeecbadedac913a9c12a01
Morten Broesby-Olsen <morten@twcapps.com>
2018-01-31 17:20:39 +0100

    Added vuex-typescript store and small example of using the adjusted store

--- template/src/components/navbar/index.ts
+++ template/src/components/navbar/index.ts
@@ -2,31 +2,39 @@ import Vue from "vue";
 import { Component, Prop } from "vue-property-decorator";
 import * as Logger from "js-logger";
 import template from "./navbar.vue";
+import * as Store from "../../store";
 
 @Component({
   mixins: [template],
 })
 export default class NavBar extends Vue {
-
-  collapsed: boolean = true;
-
-  me: "me";
-  isLoggedIn: "loggedIn";
-
-  collapse () {
-    this.collapsed = !this.collapsed;
+  /*************************************************/
+  /* LIFE CYCLE EVENTS */
+  /*************************************************/
+  created() {
+    // When the component gets created.
   }
 
-  onClickLogin () {
-    this.$store.commit("OPEN_DIALOG", "LoginModal", {});
+  mounted() {
+    // See: https://vuejs.org/v2/guide/instance.html#Instance-Lifecycle-Hooks
   }
 
-  // ...mapActions({
-  //   getAccount: 'getAccount'
-  // })
+  /*************************************************/
+  /* COMPUTED'S */
+  /*************************************************/
+  get isLoggedIn(): boolean {
+    return Store.readLoggedInState(this.$store);
+  }
 
-  created () {
-    // this.isLoggedIn && this.getAccount({id: 'me'})
-    // .catch(() => {});
+  get loginButtonText(): string {
+    return this.isLoggedIn ? "Logout" : "Login";
+  }
+
+  /*************************************************/
+  /* METHODS */
+  /*************************************************/
+  onClickLoginDemo() {
+    Store.commitLoggedInState(this.$store, !this.isLoggedIn);
+    console.log("User is logged in: ", this.isLoggedIn);
   }
 }
